add01ce70bc1a6482d25ea3f4401cc2ca081677a
mort5161 <mnielsen@esri.com>
2014-11-14 17:51:46 -0800

    Updated nuget packaging (include xml doc)

--- Tools/Nuget/buildNuget.cmd
+++ Tools/Nuget/buildNuget.cmd
@@ -4,8 +4,11 @@
 @echo * COPYING BINARIES FOR NUGET              *
 @echo *******************************************
 xcopy ..\..\src\bin\Release\NmeaParser.WinStore.dll .\NmeaParser\lib\netcore45\ /Y
+xcopy ..\..\src\bin\Release\NmeaParser.WinStore.xml .\NmeaParser\lib\netcore45\ /Y
 xcopy ..\..\src\bin\Release\NmeaParser.WinPhone.dll .\NmeaParser\lib\wpa\ /Y
+xcopy ..\..\src\bin\Release\NmeaParser.WinPhone.xml .\NmeaParser\lib\wpa\ /Y
 xcopy ..\..\src\bin\Release\NmeaParser.WinDesktop.dll .\NmeaParser\lib\net40-client\ /Y
+xcopy ..\..\src\bin\Release\NmeaParser.WinDesktop.xml .\NmeaParser\lib\net40-client\ /Y
 
 
 @echo *******************************************
