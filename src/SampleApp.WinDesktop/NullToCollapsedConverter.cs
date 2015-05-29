d3c3c6d90a213df90d63afa736c2fab19475926f
Morten <mnielsen@esri.com>
2015-05-29 09:50:14 -0700

    Hide message tiles that doesn't have data available

---
+++ src/SampleApp.WinDesktop/NullToCollapsedConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SampleApp.WinDesktop
+{
+	public sealed class NullToCollapsedConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return (value == null) ? Visibility.Collapsed : Visibility.Visible;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
