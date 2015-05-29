c323d8463572941d256a3a2de8361f8b4b597b5e
Morten Nielsen <mnielsen@esri.com>
2015-05-29 15:07:58 -0700

    Added support for Windows 10 and the new NmeaParser for 'SerialDevice'

---
+++ src/NmeaParser.UWP/SerialPortDevice.cs
@@ -0,0 +1,88 @@
+﻿//
+// Copyright (c) 2014 Morten Nielsen
+//
+// Licensed under the Microsoft Public License (Ms-PL) (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://opensource.org/licenses/Ms-PL.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Devices.SerialCommunication;
+
+namespace NmeaParser
+{
+	/// <summary>
+	/// A Serial Port NMEA device
+	/// </summary>
+	public class SerialPortDevice : NmeaDevice
+	{
+		private SerialDevice m_port;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SerialPortDevice" /> class.
+		/// </summary>
+		/// <param name="device">The serial port.</param>
+		/// <exception cref="System.ArgumentNullException">port</exception>
+		public SerialPortDevice(SerialDevice device)
+		{
+			if (device == null)
+				throw new ArgumentNullException("device");
+			m_port = device;
+		}
+
+		/// <summary>
+		/// Gets the active serial port.
+		/// </summary>
+		public SerialDevice SerialDevice
+		{
+			get
+			{
+				return m_port;
+			}
+		}
+
+		/// <summary>
+		/// Creates the stream the NmeaDevice is working on top off.
+		/// </summary>
+		/// <returns></returns>
+		protected override Task<System.IO.Stream> OpenStreamAsync()
+		{
+			return Task.FromResult<System.IO.Stream>(m_port.InputStream.AsStreamForRead());
+		}
+
+		/// <summary>
+		/// Closes the stream the NmeaDevice is working on top off.
+		/// </summary>
+		/// <param name="stream">The stream.</param>
+		/// <returns></returns>
+		protected override Task CloseStreamAsync(System.IO.Stream stream)
+		{
+			return Task.FromResult(true);
+		}
+
+		/// <summary>
+		/// Writes data to the serial port (useful for RTCM/dGPS scenarios)
+		/// </summary>
+		/// <param name="buffer">The byte array that contains the data to write to the port.</param>
+		/// <param name="offset">The zero-based byte offset in the buffer parameter at which to begin copying 
+		/// bytes to the port.</param>
+		/// <param name="count">The number of bytes to write.</param>
+		public void Write(byte[] buffer, int offset, int count)
+		{
+			m_port.OutputStream.AsStreamForWrite().Write(buffer, offset, count);
+		}
+	}
+}
--- src/SampleApp.UWP/Properties/AssemblyInfo.cs
+++ src/SampleApp.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NmeaParser")]
+[assembly: AssemblyTitle("SampleApp.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Morten Nielsen")]
-[assembly: AssemblyProduct("NmeaParser")]
-[assembly: AssemblyCopyright("Copyright © Morten Nielsen 2015")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleApp.UWP")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
