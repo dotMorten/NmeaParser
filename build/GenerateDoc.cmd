d8be33581db4cadecb26644be545fb78540a10eb
Morten Nielsen <mn@iter.dk>
2020-01-19 18:36:14 -0800

    Move the docs folder to docfx

--- build/GenerateDoc.cmd
+++ build/GenerateDoc.cmd
@@ -7,7 +7,7 @@ REM Download DocFx
 powershell -ExecutionPolicy ByPass -command "./DownloadDocFX.ps1" -Version %DocFXVersion% -Folder %DocFxFolder%
 
 REM Build metadata for all platforms (uses configuration in docfx.json in this folder)
-%DocFxFolder%\v%DocFXVersion%\docfx.exe ..\docs\docfx.json metadata
+%DocFxFolder%\v%DocFXVersion%\docfx.exe ..\docfx\docfx.json metadata
 
 REM Merge output (not implemented)
 REM %DocFxFolder%\v%DocFXVersion%\docfx.exe merge
@@ -17,4 +17,4 @@ dotnet tool install --global dotMorten.OmdGenerator
 generateomd /source=../src/NmeaParser /output=../artifacts/docs/api/omd.html
 
 REM Build the output site (HTML) from the generated metadata and input files (uses configuration in docfx.json in this folder)
-%DocFxFolder%\v%DocFXVersion%\docfx.exe ..\docs\docfx.json build
+%DocFxFolder%\v%DocFXVersion%\docfx.exe ..\docfx\docfx.json build
