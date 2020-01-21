ac9d5f6fe0f55f2c1b61c0c8352251dbb60ea135
Morten <mn@iter.dk>
2020-01-20 20:28:59 -0800

    Fix relative paths in doc script

--- build/GenerateDoc.cmd
+++ build/GenerateDoc.cmd
@@ -1,20 +1,20 @@
 @ECHO OFF
 
 SET DocFXVersion=2.48.1
-SET DocFxFolder=..\artifacts\toolset
+SET DocFxFolder=%~dp0\..\artifacts\toolset
 
 REM Download DocFx
-powershell -ExecutionPolicy ByPass -command "./DownloadDocFX.ps1" -Version %DocFXVersion% -Folder %DocFxFolder%
+powershell -ExecutionPolicy ByPass -command "%~dp0/DownloadDocFX.ps1" -Version %DocFXVersion% -Folder %DocFxFolder%
 
 REM Build metadata for all platforms (uses configuration in docfx.json in this folder)
-%DocFxFolder%\v%DocFXVersion%\docfx.exe ..\docs\docfx.json metadata
+%DocFxFolder%\v%DocFXVersion%\docfx.exe %~dp0..\docs\docfx.json metadata
 
 REM Merge output (not implemented)
 REM %DocFxFolder%\v%DocFXVersion%\docfx.exe merge
 
 REM Generate OMD
 dotnet tool install --global dotMorten.OmdGenerator
-generateomd /source=../src/NmeaParser /output=../artifacts/docs/api/omd.html
+generateomd /source=%~dp0../src/NmeaParser /output=%~dp0../artifacts/docs/api/omd.html
 
 REM Build the output site (HTML) from the generated metadata and input files (uses configuration in docfx.json in this folder)
-%DocFxFolder%\v%DocFXVersion%\docfx.exe ..\docs\docfx.json build
+%DocFxFolder%\v%DocFXVersion%\docfx.exe %~dp0..\docs\docfx.json build
