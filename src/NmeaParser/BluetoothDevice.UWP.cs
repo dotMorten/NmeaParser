d37bd9af0898fa81c891f0415a626b68b1982da6
Morten Nielsen <mort5161@esri.com>
2024-07-02 15:44:46 -0700

    Add BT device to net6-windows

--- src/NmeaParser/BluetoothDevice.UWP.cs
+++ src/NmeaParser/BluetoothDevice.UWP.cs
@@ -12,7 +12,7 @@
 //  *   limitations under the License.
 //  ******************************************************************************
 
-#if WINDOWS_UWP
+#if WINDOWS_UWP || WINDOWS
 using System;
 using System.Collections.Generic;
 using System.Linq;
