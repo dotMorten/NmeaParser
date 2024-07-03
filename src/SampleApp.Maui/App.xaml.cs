59cbcc761d06cdb03332d6df0a28feebdc6781bf
Morten Nielsen <mort5161@esri.com>
2024-07-03 12:41:49 -0700

    Add MAUI sample and delete Android

---
+++ src/SampleApp.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleApp.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
